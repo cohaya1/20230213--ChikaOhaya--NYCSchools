0d10ff47a8f1c14641af1fea3cc3b6bf8d807f2f
Chika Ohaya <chika.ohaya@gmail.com>
2023-02-13 11:17:18 -0500

    Initial Commit

---
+++ SchoolSystem/SchoolSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolSystemApp.swift
+//  SchoolSystem
+//
+//  Created by Chika Ohaya on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SchoolSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
